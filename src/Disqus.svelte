e9e7e71a6bd559405406f023dc9c354cd6e90dc9
Mudassar <demoncious@gmail.com>
2020-05-11 09:32:30 -0700

    Ported to Svelte Actions

--- src/Disqus.svelte
+++ src/Disqus.svelte
@@ -1,29 +1,12 @@
 <script>
-    import { onMount } from "svelte";
-    
-    export let url = window.location.href;
-    export let container = "disqus_container";
-    export let identifier;
+    import DisqusThread from "./action.js";
 
-    onMount(() => {
-        if(url && identifier) {
-            const config = document.createElement('script');
-            config.type = 'text/javascript';
-            config.innerHTML = 
-            `const disqus_config = function () {
-                this.page.url = '${url}';
-                this.page.identifier = '${identifier}';
-            };`;
-            document.head.appendChild(config);
-
-            const disqus = document.createElement('script');
-            disqus.src = 'https://svelte-test.disqus.com/embed.js';
-            disqus.setAttribute('data-timestamp', +new Date());
-            (document.head || document.body).appendChild(disqus);
-        } else console.error("In-sufficient Props for disqus-svelte Comments.\n\n The 'identifier' property containing a Unique Identifier for the current page is Required!");
-    });
+    export let container  = "disqus_container";
+    export let url        = window.location.href;
+    export let identifier = window.location.pathname;
 </script>
 
-<div class={container}>
-    <div id="disqus_thread"></div>
-</div>
+<div use:DisqusThread={{
+    identifier,
+    url
+}} class={container} />
